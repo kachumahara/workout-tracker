392db397e13feb05f1a9a7e742197980f2be130b
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-27 11:38:31 -0700

    good

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+let moment = require("moment");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -9,11 +9,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
+
+// using predefined format string'tiny'
+app.use("morgan")
+
+// parse requests as JSON 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// serving static content for app from public __dir
 app.use(express.static('public'));
+
 //connection to mongodb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
+
+//html
+app.get()
